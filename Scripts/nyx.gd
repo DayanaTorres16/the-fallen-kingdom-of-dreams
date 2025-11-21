94e0f25b7e58741c6d3f651f8df721b4c6b7636f
Dayana Torres <141515422+DayanaTorres16@users.noreply.github.com>
2025-11-20 23:37:37 -0500

    Tutorial Progress Instructions

--- Scripts/nyx.gd
+++ Scripts/nyx.gd
@@ -1,54 +1,48 @@
 extends CharacterBody2D
 
-
 signal doble_salto_ejecutado 
 
-const SPEED = 250.0 
+const SPEED = 350.0 
 const JUMP_VELOCITY = -450.0  
-const DOUBLE_JUMP_VELOCITY = -400.0 
+const DOUBLE_JUMP_VELOCITY = -350.0 
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") 
+@onready var sprite_2d = $AnimatedSprite2D 
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_count = 0
 const MAX_JUMPS = 2
 
-@onready var animated_sprite = $AnimatedSprite2D 
-
 func _physics_process(delta):
-	var velocity = self.velocity
-
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "Run"
+	else:
+		sprite_2d.animation = "Idle"
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.y < 0:
+			sprite_2d.animation = "Jump"
+		else:
+			sprite_2d.animation = "Fall"
 	else:
 		jump_count = 0 
 	
-	if Input.is_action_just_pressed("Jump"): 
-		
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			jump_count += 1
-			
-		elif jump_count < MAX_JUMPS:
-			velocity.y = DOUBLE_JUMP_VELOCITY
-			jump_count += 1
-			
-			emit_signal("doble_salto_ejecutado")
-
-	var direction = Input.get_axis("Left", "Right") 
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 	
+	elif Input.is_action_just_pressed("Jump") and jump_count < MAX_JUMPS and not is_on_floor():
+		velocity.y = DOUBLE_JUMP_VELOCITY
+		jump_count += 1
+		emit_signal("doble_salto_ejecutado")
+	
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite.flip_h = direction < 0
-		animated_sprite.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * 4 * delta)
-		if is_on_floor():
-			animated_sprite.play("Idle")
-
-	if not is_on_floor():
-		if velocity.y < 0:
-			animated_sprite.play("Jump") 
-		else:
-			animated_sprite.play("Fall") 
-
-	self.velocity = velocity
+		velocity.x = move_toward(velocity.x, 0, 10)
+	
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
